b93f3342701299699ad0b5bf9fb4b42dfeab67b3
Shilpi sharma <sap.developer.shilpi@gmail.com>
2017-09-18 19:06:49 +0000

    edit and update actions created and edit view also generated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,9 +11,21 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+  def update
+    @article= Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice]= "Article is updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
   def show
     @article= Article.find(params[:id])
   end 
+  def edit
+    @article=Article.find(params[:id])
+  end
   
   private
   def article_params
