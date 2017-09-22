ef12d19205e23a8d2b25ccbe78bc800d691c65b9
Shilpi sharma <sap.developer.shilpi@gmail.com>
2017-09-22 11:32:03 +0000

    add edit update functionality and partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,9 +7,9 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   def edit
+    @article = Article.find(params[:id])
   end
   def create
-    debugger
     @article = Article.new(article_params)
     @article.user= User.first
     if @article.save
@@ -20,6 +20,7 @@ class ArticlesController < ApplicationController
     end
   end
   def update
+    @article.user= User.first
     if @article.update(article_params)
       flash[:success]= "Article is updated"
       redirect_to article_path(@article)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :show, :destroy, :update]
   def new
     @user = User.new
   end
@@ -11,8 +12,22 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  def edit
+  end
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Information updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
   
-  private 
+  private
+  
+  def set_user
+    @user=User.find(params[:id])
+  end
   
   def user_params
     params.require(:user).permit(:username, :email, :password)
