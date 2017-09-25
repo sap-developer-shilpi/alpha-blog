4f3c055849bff174c12797d78c2bb64d38aca792
Shilpi sharma <sap.developer.shilpi@gmail.com>
2017-09-25 13:31:59 +0000

    complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :show, :destroy, :update]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   def index
     @articles=Article.paginate(page: params[:page], per_page: 5)
   end
@@ -41,5 +43,11 @@ class ArticlesController < ApplicationController
   end
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger]= "you can only edit or delete your own articles"
+      redirect_to root_path
+    end
   end
 end  
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :show, :destroy, :update]
+  before_action :require_same_user, only: [:edit, :update,]
+  
   def new
     @user = User.new
   end
+  
   def index
     @users= User.paginate(page: params[:page], per_page: 5)
   end
+  
   def create
     @user= User.new(user_params)
     if @user.save
@@ -15,8 +19,10 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
   def edit
   end
+  
   def update
     if @user.update(user_params)
       flash[:success] = "Information updated"
@@ -25,6 +31,7 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
   def show
     @user_article = @user.articles.paginate(page: params[:page], per_page: 5)
   end
@@ -35,7 +42,15 @@ class UsersController < ApplicationController
     @user=User.find(params[:id])
   end
   
+  def require_same_user
+   if current_user!= @user
+     flash[:danger]="You can only edit your own account"
+      redirect_to root_path
+   end  
+  end
+  
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+  
 end
