775d135eaf6997835d0a8b9b33223a971586b422
Shilpi sharma <sap.developer.shilpi@gmail.com>
2017-09-29 12:40:39 +0000

    add article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true, length: {minimum: 4, maximun: 10}
   validates :description, presence: true, length: {minimum: 15, maximun: 100}
   validates :user_id, presence: true
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
+  has_many :article_categories
+  has_many :articles, through: :article_categories
   validates :name, presence: true, length: {maximum: 25, minimum: 3} 
   validates :name, uniqueness: {case_sensitive: false}
 end
